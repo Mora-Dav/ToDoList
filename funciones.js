64387387188320b812c23ca7ec30f3dcf5d1e2b3
Mora-Dav <moradavi@gmail.com>
2021-08-07 00:13:28 -0300

    agrega funcionalidad para sobreescribir json

--- funciones.js
+++ funciones.js
@@ -1,13 +1,34 @@
 const fs = require('fs');
-const data = fs.readFileSync('tareas.json', 'utf-8')
+
+/*const data = fs.readFileSync('tareas.json', 'utf-8')
 
 const tareas = JSON.parse(data);
-
+*/
 
 
 const funciones = {
+
+    leerJSON: function () {
+        return JSON.parse(fs.readFileSync('tareas.json', 'utf-8'))
+    },
+
+    escribirJSON: function(tareas) {
+        return fs.writeFileSync('tareas.json', JSON.stringify(tareas, null, ' '))
+    }
+
     listar () {
         console.log('El usuario eligió listar');
+        /*
+        Llamar a la funcion leerJSON
+        Guardar la info de la llamada en una variable
+        Recorrer y consologuear la info (forEach)
+        Darle formato a la informacion
+        */
+       const tareas= this.leerJSON()
+       tareas.forEach((tarea, i) => {
+           console.log(i+1 + ')'+ tarea.titulo + ' - '+tarea.estado)
+           
+       });
     },
     agregar() {
         console.log('El usuario eligió agregar');
